8293f78b12aea6aa7236dfe212e7e92a42825586
GiaBao2001zz <tranhoanggiabao2001@gmail.com>
2021-06-17 20:58:11 +0700

    Add UI add food

---
+++ app/src/main/java/com/example/orderfoodappforenterprise/AddFood.kt
@@ -0,0 +1,11 @@
+package com.example.orderfoodappforenterprise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddFood : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_food)
+    }
+}
